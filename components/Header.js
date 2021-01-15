3862598f916fe7064f7d3756af46e394f2830fa1
ahmedseragcodes <ahmedserag1@gmail.com>
2021-01-15 11:20:55 -0500

    finished header

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,5 @@
+
+
 // STEP 1: Create a Header component.
 // -----------------------
 // Write a function that takes no arguments and returns the markup you see below:
@@ -11,4 +13,34 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    
+    //Header Container 
+    const headerContainer=document.querySelector(".header-container");
+    
+    //Header DIV
+    const headerDiv=document.createElement("div");
+    headerDiv.classList.add("header");
+    headerContainer.append(headerDiv);
+
+    //Date Span
+    const dateSpan=document.createElement("span");
+    dateSpan.classList.add("date");
+    dateSpan.textContent="March 28, 2020";
+    headerDiv.append(dateSpan);
+
+    //Title Heading
+    const titleHeading=document.createElement("h1");
+    titleHeading.textContent="Lambda Times";
+    headerDiv.append(titleHeading);
+
+    //Temperature Span
+    const tempSpan=document.createElement("span");
+    tempSpan.classList.add("temp");
+    tempSpan.textContent="98 Degrees";
+    headerDiv.append(tempSpan);
+
+}
+
+//Invocation 
+Header();
